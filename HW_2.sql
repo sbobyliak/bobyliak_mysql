9b77cffca95f502efb72f135e51bca20826e06b8
sbobyliak <sbobyliak@kse.org.ua>
2025-10-24 16:35:40 +0300

    Optimize database schema and queries in HW_2.sql
    Added primary keys and indexes to optimize queries.

--- HW_2.sql
+++ HW_2.sql
@@ -2,6 +2,7 @@ CREATE DATABASE Optimization;
 USE Optimization;
 
 -- NOToptimized (AI-generated) query
+EXPLAIN ANALYZE
 SELECT
   c.customer_id,
   c.first_name,
@@ -20,6 +21,23 @@ HAVING total_orders > 0
 ORDER BY total_spent DESC;
 
 -- optymized query
+ALTER TABLE customers 
+ADD PRIMARY KEY (customer_id);
+
+ALTER TABLE products 
+ADD PRIMARY KEY (product_id);
+
+ALTER TABLE orders 
+ADD PRIMARY KEY (order_id);
+
+ALTER TABLE customers MODIFY city VARCHAR(100);
+ALTER TABLE orders MODIFY status VARCHAR(20);
+
+CREATE INDEX idx_customers_city ON customers(city);
+CREATE INDEX idx_products_id_price ON products(product_id, price);
+CREATE INDEX idx_orders_status_customer ON orders(status, customer_id);
+
+EXPLAIN ANALYZE
 WITH filtered_orders AS (
     SELECT o.customer_id, o.product_id, o.quantity, o.order_date
     FROM orders o
@@ -33,7 +51,7 @@ SELECT f.customer_id,
     SUM(f.quantity * p.price) AS total_spent,
     MAX(f.order_date) AS last_order_date
 FROM filtered_orders f
-JOIN customers c USE INDEX (PRIMARY) ON f.customer_id = c.customer_id
+JOIN customers c  ON f.customer_id = c.customer_id
 JOIN products p USE INDEX (idx_products_id_price) ON f.product_id = p.product_id
 GROUP BY f.customer_id, c.first_name, c.last_name, c.city
 ORDER BY total_spent DESC;
